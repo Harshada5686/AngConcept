50a03d10add1ce0d5cd80b21e5c991fb4adbf8a9
Harshada5686 <harshadawagh5686@gmail.com>
2025-01-03 18:31:49 +0530

    updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'app',component:AppComponent},
+    {path:'home',component:HomeComponent},
+    {path:'login',component:LoginComponent}
+];
---
+++ src/app/component/cakecard/cakecard.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-cakecard',
+  standalone: true,
+  imports: [],
+  templateUrl: './cakecard.component.html',
+  styleUrl: './cakecard.component.scss'
+})
+export class CakecardComponent {
+  @Input() product:any;
+}
