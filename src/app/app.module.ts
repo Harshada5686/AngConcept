1a7ad1efb76693570e534c15e9d21ca612b25dc6
Harshada5686 <harshadawagh5686@gmail.com>
2025-01-06 23:01:26 +0530

    mat label

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
+import { LoginComponent } from './component/login/login.component';
+import { CakecardComponent } from './component/cakecard/cakecard.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent // Add other components here
+    AppComponent,
+    LoginComponent,
+    CakecardComponent,
+    FooterComponent,
+    HeaderComponent,
+
   ],
   imports: [
-    BrowserModule,MatDialogModule,MatFormFieldModule,FormsModule, MatIconModule,MatSelectModule, MatRadioModule, ReactiveFormsModule
+    BrowserModule,MatDialogModule,MatFormFieldModule,FormsModule, MatIconModule,MatSelectModule, MatRadioModule, ReactiveFormsModule,FormBuilder,FormControl,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
